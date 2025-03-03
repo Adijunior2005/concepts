b802fed225bf9f3a36ea00ffe487bad0a801ada0
ADITYA YADAV <adityasuhas123@gmail.com>
2025-03-03 23:53:57 +0530

    tree updation

--- tree_operations.cpp
+++ tree_operations.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
- struct node{
+struct node{
     
     int data;
     struct node* left;
@@ -24,15 +24,49 @@ using namespace std;
      
  }
 
+ void inorder(struct node* root){
+    if ( root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+ }
+
+ void postorder( struct node* root){
+    if (root==NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+ }
+ /*      TREE
+
+          1
+         / \
+        2    3
+       / \  / \
+      4   5 6  7
+
+*/
+
 int main(){
     struct node* root=new node(1);
     root->left = new node(2);
-    root->left = new node(3);
+    root->right = new node(3);
     root->left->left = new node(4);
     root->left->right = new node(5);
     root->right->left = new node(6);
     root->right->right = new node(7);
+    cout<<"preorder"<<endl;
     preorder(root);
-    cout<<"hi";
+    cout<<endl;
+    cout<<"inorder"<<endl;
+    inorder(root);
+    cout<<endl;
+    cout<<"postorder"<<endl;
+    postorder(root);
+    
     return 0;
 }
